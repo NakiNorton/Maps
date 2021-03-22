a749e4d384c6b95f111810e251430cc9f0ea5b71
Stephanie Norton <steph.jane.norton@gmail.com>
2021-03-22 17:51:32 -0600

    Add error message to display w, conditional render

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -39,38 +39,56 @@ class Form extends Component {
 
   render() {
     return (
-      <form className="form">
-        <label>
-          Name
-          <input
-            ref={(input) => { this.name = input }}
-            type="text"
-          />
-        </label>
-        <label>
-          Lat
-          <input
-            ref={(input) => { this.lat = input }}
-            type="text"
-          />
-        </label>
-        <label>
-          Lon
-          <input
-            ref={(input) => { this.lng = input }}
-            type="text"/>
-        </label>
-        <button
-          type="submit"
-          onClick={(e) => this.submitForm(e, {
-            name: this.name.value,
-            lat: this.lat.value,
-            lng: this.lng.value
-          })}
-        >
+        <form className="form">
+          <label>
+            Name
+            <input
+              ref={(input) => { this.name = input }}
+              type="text"
+            />
+          </label>
+          <label>
+            Lat
+            <input
+              ref={(input) => { this.lat = input }}
+              type="text"
+            />
+          </label>
+          <label>
+            Lon
+            <input
+              ref={(input) => { this.lng = input }}
+              type="text"/>
+          </label>
+          <button
+            type="submit"
+            onClick={(e) => this.submitForm(e, {
+              name: this.name.value,
+              lat: this.lat.value,
+              lng: this.lng.value
+            })}
+          >
             Save
-        </button>
-      </form>
+          </button>
+          {this.state.errorMsg &&
+          <section style={{ textAlign: 'left' }}> 
+            <p style={{ padding: '.25rem 1rem' }}>
+              <span style={{ color: 'red' }}>Sorry! Something went wrong.</span><br/><br/>Please make sure the name and coordinates are valid:
+            </p>
+            <ul>
+              <li>
+                <b>Name</b> needs to be between 1-25 characters, and only include letters, apostrophees, periods & hyphens.
+              </li>
+              <li>
+                <b>Latitude</b> must be a number between -90 & 90 (inclusive).
+              </li>
+              <li>
+                <b>Longitude</b> must be a number between -180 & 180 degrees (inclusive).
+              </li>
+            </ul>
+          </section>
+          }
+        </form>
     );
   }
 }
