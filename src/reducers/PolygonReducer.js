85579bc758041f9e742b8cac5d5530fa78ca2ada
Stephanie Norton <steph.jane.norton@gmail.com>
2021-03-23 17:07:49 -0600

    Fix error creating duplicates

--- src/reducers/PolygonReducer.js
+++ src/reducers/PolygonReducer.js
@@ -2,12 +2,10 @@ const initialState = {
   data: []
 }
 
-
 const PolygonMarkers = (state = initialState, action) => {
   switch (action.type) {
     case 'STORE_POLYGON_COORDINATES':
-      console.log(action.data)
-      const coordinates = {...state, data: state.data.concat(action.data)}
+      const coordinates = {...state, data: action.data}
       return coordinates
     default:
       return state;
